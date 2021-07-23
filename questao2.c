d6419d9aca996d0495087d4f21642390036140ef
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2021-07-22 21:24:13 -0400

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -5,44 +5,38 @@ int uniao(int *vetor1, int a, int *vetor2, int b, int *vetor3);
 
 int main(){
 	int i, a, b, count, *vetor3;
-
 	scanf("%d %d", &a, &b);
-
 	int vetor1[a], vetor2[b];
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < a; i++){
 		scanf("%d", &vetor1[i]);
-
-	for (i = 0; i < b; i++)
+        }
+	for (i = 0; i < b; i++){
 		scanf("%d", &vetor2[i]);
-
+        }
 	vetor3 = (int*) malloc((a + b) * sizeof(int));
 
 	if (vetor3 == NULL) {
 		fprintf(stderr, "error: memory not allocated");
 		return 1;
 	}
-	
 	count = uniao(vetor1, a, vetor2, b, vetor3);
-
-	for (i = 0; i < count; i++)
+	for (i = 0; i < count; i++){
 		printf("%d\n", *(vetor3 + i));
-
+        }
 	free(vetor3);
 
 	return 0;
 }
 
-int
-uniao(int *vetor1, int a, int *vetor2, int b, int *vetor3)
-{
+int uniao(int *vetor1, int a, int *vetor2, int b, int *vetor3){
 	int i;
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < a; i++){
 		*(vetor3 + i) = vetor1[i];
-
-	for (i = 0; i < b; i++)
+        }
+	for (i = 0; i < b; i++){
 		*(vetor3 + a + i) = vetor2[i];
-
+        }
 	return a + b;
 }
