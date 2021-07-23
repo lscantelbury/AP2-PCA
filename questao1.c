4110725f1b27659491129e274fe469070a88a5f4
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2021-07-22 21:21:05 -0400

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
 #define MAX 150
 
-int
-has_val(int *vec, int size, int val){
+int has_val(int *vec, int size, int val){
 	int i;
-
 	for (i = 0; i < size; i++){
-		if (vec[i] == val)
+		if (vec[i] == val){
 			return 1;
+		}
 	}
-
 	return 0;
 }
 
-int
-intersec(int *vec1, int *vec2, int *res)
-{
-	int i, count = 0;
-
+int intersec(int *vec1, int *vec2, int *res){
+	int i;
+	int count = 0;
 	for (i = 0; i < MAX; i++){
 		if (has_val(vec2, MAX, vec1[i])){
 			res[count] = vec1[i];
@@ -28,20 +23,19 @@ intersec(int *vec1, int *vec2, int *res)
 
 	return count;
 }
-
 int main(){
-	int i, aux, vetor1[MAX], vetor2[MAX], vetor3[MAX];
-
+	int i, aux; 
+	int vetor1[MAX], vetor2[MAX], vetor3[MAX];
 	for (i = 0; i < MAX; i++){
 		scanf("%d", vetor1 + i);
-}
+        }
 	for (i = 0; i < MAX; i++){
 		scanf("%d", vetor2 + i);
-}
+        }
 	aux = intersec(vetor1, vetor2, vetor3);
 
 	for (i = 0; i < aux; i++){
 		printf("%d\n", vetor3[i]);
-}
+        }
 	return 0;
 }
